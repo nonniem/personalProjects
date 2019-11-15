2769a3f140e4a5c6ce3e06d87aa9c6d77f58c466
Nonnie <martin.nonnie@gmail.com>
2019-11-15 10:27:37 -0600

    background color changes

--- portfolio/src/main/Navbar.js
+++ portfolio/src/main/Navbar.js
@@ -4,9 +4,7 @@ import {Link} from "react-router-dom";
 function Navbar(props){
   const footer = {
       color: "black",
-      height: "8vh",
-      // background: "url('https://media.giphy.com/media/aRZ4vTsHnyW6A/giphy.gif')",
-      // opacity: ".65",
+      height: "10vh",
       position: "relative",
       zIndex: "1"
   }
@@ -16,8 +14,9 @@ function Navbar(props){
       justifyContent: "space-around",
       alignItems: "center",
       textShadow: "-1px 0 #19EBFF, 0 1px red, 1px 0 black, 0 -1px blue",
-      fontSize: "2em",
+      fontSize: "3em"
   }
+
     return(
         <div>
             <div>
@@ -26,14 +25,10 @@ function Navbar(props){
                     <Link to="/" className="navbarLink">Bio</Link>
                     <Link to="/resume" className="navbarLink">Resume</Link>
                     <Link to="/education" className="navbarLink">Education</Link>
-
                     <a style={{...divd, ...footer}} href="https://github.com/nonniem"><i className="fa fa-github" aria-hidden="false"></i></a>
-                      <a style={{...divd, ...footer}} href="https://www.linkedin.com/in/nonnie-martin-019847149/"><i className="fa fa-linkedin" aria-hidden="false"></i></a>
+                    <a style={{...divd, ...footer}} href="https://www.linkedin.com/in/nonnie-martin-019847149/"><i className="fa fa-linkedin" aria-hidden="false"></i></a>
                     <a style={{...divd, ...footer}} href="https://www.facebook.com/bigcheifmartin"><i className="fa fa-facebook" aria-hidden="false"></i></a>
-
-
                 </div>
-
             </div>
         </div>
     )
