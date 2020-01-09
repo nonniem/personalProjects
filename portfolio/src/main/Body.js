8539d4cfc390c7192032546f7a923cb74f06bd9a
Nonnie <martin.nonnie@gmail.com>
2020-01-09 10:01:54 -0600

    took off the rocket :p

--- portfolio/src/main/Body.js
+++ portfolio/src/main/Body.js
@@ -15,37 +15,5 @@ function Body(props){
                 <Route path="/resume" component={Resume}/>
                 <Route path="/" component={Contact}/>
             </Switch>
-            <div className="rocket">
-   <div className="rocket-body">
-     <div className="body"></div>
-     <div className="fin fin-left"></div>
-     <div className="fin fin-right"></div>
-     <div className="window"></div>
-   </div>
-   <div className="exhaust-flame"></div>
-   <ul className="exhaust-fumes">
-     <li></li>
-     <li></li>
-     <li></li>
-     <li></li>
-     <li></li>
-     <li></li>
-     <li></li>
-     <li></li>
-     <li></li>
-   </ul>
-   <ul className="star">
-     <li></li>
-     <li></li>
-     <li></li>
-     <li></li>
-     <li></li>
-     <li></li>
-     <li></li>
-   </ul>
- </div>
-        </div>
-    )
-}
-
+          </div> )}
 export default Body;
